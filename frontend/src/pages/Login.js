7a7247a70425ad8b62c2bd98bcd9c3e3dde112d1
fbayhan <fatih-bayhan@windowslive.com>
2024-09-12 22:42:40 +0300

    login arayüz güncellendi

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import NavBar from "../component/NavBar";
-
+import LoginComponent from "../component/LoginComponent";
 const Login = () => {
     const navigate = useNavigate();
     return (
         <div className="container">
-            <NavBar></NavBar>
-            Login
+            <LoginComponent></LoginComponent>
         </div>
     );
 };
